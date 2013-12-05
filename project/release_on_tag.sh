05956098a1da6b08940d48a7be1b0c11d35d457b
Adriaan Moors <adriaan.moors@typesafe.com>
2013-12-05 14:46:07 -0800

    Disable openjdk7 (reduce load). Prep tag-driven releasing.

---
+++ project/release_on_tag.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+# if the current commit has a tag named like v(\d+\.\d+\.\d+.*),
+# and we're running on the right jdk/branch,
+# echo the sbt commands that publish a release with the version derived from the tag
+publishJdk=openjdk6
+publishBranch=master
+publishScalaVersion=2.11.0-M7
+
+unset tag version
+
+# Exit without error when no (annotated) tag is found.
+tag=$(git describe HEAD --exact-match 2>/dev/null || exit 0)
+
+version=$(echo $tag | perl -pe 's/v(\d+\.\d+\.\d+.*)/$1/')
+
+if [[ "$version"                !=  "" &&\
+      "${TRAVIS_PULL_REQUEST}"  == "false" &&\
+      "${JAVA_HOME}"            == "$(jdk_switcher home $publishJdk)" &&\
+      "${TRAVIS_BRANCH}"        == "${publishBranch}" &&\
+      "${TRAVIS_SCALA_VERSION}" == "${publishScalaVersion}" ]]; then
+  echo \'"set every version := $version"\' publish-signed
+fi
