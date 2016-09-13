3af124800fe01bb12919de3c8b15211305ea5042
Daniil Smirnov <danslapman@gmail.com>
2016-09-14 00:04:17 +0300

    Rewrite for-comprehension to avoid calling .withFilter

--- library/src/test/scala/scala/tools/selectivecps/TestSuite.scala
+++ library/src/test/scala/scala/tools/selectivecps/TestSuite.scala
@@ -661,12 +661,8 @@ class HigherOrder {
   @Test def t5472 = {
     val map = Map("foo" -> 1, "bar" -> 2)
     reset {
-      val mapped =
-        for {
-          (location, accessors) <- new ContinuationizedParallelIterable(map)
-        } yield {
-          shiftUnit0[Int, Unit](23)
-        }
+      val mapped = new ContinuationizedParallelIterable(map)
+        .map(strIntTpl => shiftUnit0[Int, Unit](23))
       assertEquals(List(23, 23), mapped.toList)
     }
   }
